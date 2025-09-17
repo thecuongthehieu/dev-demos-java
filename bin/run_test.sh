08d923d4cc02add6720f666317563c9c6444414e
thecuongthehieu <thecuongthehieu@gmail.com>
2025-09-17 12:20:36 +0700

    Test SimplexSolver

--- bin/run_test.sh
+++ bin/run_test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+./gradlew -x test modules:libs-test:runWithExec
+
 ./gradlew modules:libs-test:test
 
 #./gradlew modules:libs-test:test --tests libs.test.TestGuava
--- modules/libs-test/src/main/java/libs/test/Main.java
+++ modules/libs-test/src/main/java/libs/test/Main.java
@@ -1,25 +1,42 @@
 package libs.test;
 
-import com.google.common.util.concurrent.RateLimiter;
-import util.Timestamp;
+import org.apache.commons.math3.optim.linear.*;
+import org.apache.commons.math3.optim.*;
+import org.apache.commons.math3.optim.nonlinear.scalar.GoalType;
 
-import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Main {
-	public static void main(String[]args){
-		System.out.println("Here is Libs Test");
+    public static void main(String[] args) {
+        // Objective function: 3x + 5y
+        LinearObjectiveFunction objective = new LinearObjectiveFunction(new double[] {3, 5}, 0);
 
-		RateLimiter rateLimiter = RateLimiter.create(100);
+        // Constraints
+        Collection<LinearConstraint> constraints = new ArrayList<>();
+        constraints.add(new LinearConstraint(new double[] {2, 8}, Relationship.LEQ, 13));
+        constraints.add(new LinearConstraint(new double[] {5, -1}, Relationship.LEQ, 11));
+        constraints.add(new LinearConstraint(new double[] {1, 0}, Relationship.GEQ, 0));
+        constraints.add(new LinearConstraint(new double[] {0, 1}, Relationship.GEQ, 0));
 
-		Timestamp ts = new Timestamp();
+        // Solver
+        SimplexSolver solver = new SimplexSolver();
+        PointValuePair solution = solver.optimize(
+                new MaxIter(100),
+                objective,
+                new LinearConstraintSet(constraints),
+                GoalType.MAXIMIZE,
+                new NonNegativeConstraint(true)
+        );
 
-		for (int i = 0; i < 1000; ++i) {
-			rateLimiter.acquire();
-			//System.out.println(i + 1);
-		}
+        // Print results
+        double x = solution.getPoint()[0];
+        double y = solution.getPoint()[1];
+        double value = solution.getValue();
 
-		System.out.println(ts.elapsed());
-
-
-	}
+        System.out.println("Optimal Solution:");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("Max Value = " + value);
+    }
 }
